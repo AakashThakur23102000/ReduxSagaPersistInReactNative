b385afaa807c60c30d32265391b6048ca3d645d4
Aakash Thakur <aakash.thakur@sharajman.com>
2024-05-20 11:16:05 +0530

    Addded persist logic init

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import reducerStore from './src/redux/reducerStore';
 
-const newAppJs = ()=>{
-    return(
+// persistor
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const newAppJs = () => {
+    let persister = persistStore(reducerStore)
+    return (
         <Provider store={reducerStore}>
-            <App/>
+            <PersistGate persistor={persister}>
+                <App />
+            </PersistGate>
         </Provider>
     )
 }
--- src/redux/reducerStore.js
+++ src/redux/reducerStore.js
@@ -5,14 +5,56 @@ import createSageMiddleware from "redux-saga"
 
 const { configureStore } = require("@reduxjs/toolkit");
 const { default: CommentSlice } = require("./CommentSlice");
+// saga
 const sagaMiddleware = createSageMiddleware();
 
+// persisit
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+var persistConfig = {
+    key: "persistData",
+    storage: AsyncStorage
+}
+var rootReducer = combineReducers({
+    CommentSlice: CommentSlice.reducer,
+    StorySlice: StorySlice.reducer
+})
+var persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+// Persist configuration for slective feild
+// const commentPersistConfig = {
+//     key: "comment",
+//     storage: AsyncStorage,
+// };
+// const storyPersistConfig = {
+//     key: "story",
+//     storage: AsyncStorage,
+// };
+// var persistedCommentSliceReducer = persistReducer(commentPersistConfig, CommentSlice.reducer);
+// var persistedStorySliceReducer = persistReducer(storyPersistConfig, StorySlice.reducer);
+
+
+// main 
 var reducerStore = configureStore({
-    reducer: {
-        CommentSlice: CommentSlice.reducer,
-        StorySlice: StorySlice.reducer
-    },
-    middleware:()=>[sagaMiddleware]
+    // reducer: {
+    //     CommentSlice: CommentSlice.reducer,
+    //     StorySlice: StorySlice.reducer
+    // },
+
+    // normal full persist
+    reducer: persistedReducer,
+
+    // for slective feild
+    // reducer:{
+    //     //   here to make only story slice to persisit
+    //     CommentSlice:persistedCommentSliceReducer,
+    //     StorySlice:persistedStorySliceReducer
+    // },
+    middleware: () => [sagaMiddleware]
 })
 sagaMiddleware.run(StorySaga);
 sagaMiddleware.run(CommentSaga);
